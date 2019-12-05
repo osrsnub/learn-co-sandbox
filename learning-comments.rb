c4d36ddaf6da288f7c34a29ad343c824a7f3a572
Aaron Cherniavsky <aaron.b.cherniavsky@gmail.com>
2019-12-05 01:10:34 +0000

    Automatically backed up by Learn

---
+++ learning-comments.rb
@@ -0,0 +1,5 @@
+    #method name      #parameter
+def greeting_a_person(name,language)
+  puts "Hello #{name} we hear you are a great #{language} programmer!!"
+end
+greeting_a_person("Maria","python")
